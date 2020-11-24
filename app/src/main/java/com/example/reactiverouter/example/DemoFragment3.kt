b8844a21a1e907086ec68fdd6f23077ffda01d22
Vyacheslav Vorozheykin <qwe2rty1@gmail.com>
2020-11-24 21:31:12 +0700

    Expand Demo

--- app/src/main/java/com/example/reactiverouter/example/DemoFragment3.kt
+++ app/src/main/java/com/example/reactiverouter/example/DemoFragment3.kt
@@ -1,5 +1,23 @@
 package com.example.reactiverouter.example
 
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.reactiverouter.R
+import kotlin.random.Random
 
-class DemoFragment3 : Fragment()
+class DemoFragment3 : Fragment() {
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+		return inflater.inflate(R.layout.demo_fragment_3, container, false)
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		view.setBackgroundColor(
+			Color.rgb(Random.nextInt(255), Random.nextInt(255), Random.nextInt(255))
+		)
+	}
+}
