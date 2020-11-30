6cdc648d198e4985d84278abfdad259814427dd2
Vyacheslav Vorozheykin <qwe2rty1@gmail.com>
2020-11-30 23:46:09 +0700

    Process isIgnoring.

--- app/src/main/java/com/example/reactiverouter/base/ReactiveRouter.kt
+++ app/src/main/java/com/example/reactiverouter/base/ReactiveRouter.kt
@@ -154,6 +154,9 @@ abstract class ReactiveRouter<N : Navigator, SP : ScopeProvider<N>>(
 
 	private fun <T> deferScope(scope: Scope<T, N>): Single<Boolean> {
 		return Single.defer {
+			if (ignoreNextElements) {
+				Single.just(false)
+			}
 			when (scope) {
 				is Scope.Simple -> deferSimpleScope(scope)
 				is Scope.Reactive -> deferReactiveScope(scope)
@@ -204,4 +207,13 @@ abstract class ReactiveRouter<N : Navigator, SP : ScopeProvider<N>>(
 		}
 		return completable!!
 	}
+
+	private val ignoreNextElements: Boolean
+		get() = when (val lastScope = scopesQueue.lastOrNull()) {
+			null -> false
+			is Scope.Simple<*> -> lastScope.isIgnoring
+			is Scope.Reactive.Blocking<*, *> -> lastScope.isIgnoring
+			is Scope.Chain.Blocking<*> -> lastScope.isIgnoring
+			else -> false
+		}
 }
